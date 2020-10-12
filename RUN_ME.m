ca47e1ba33001664d2c9021c9b0b9de700139b60
Nathan Bamberger <38992219+nbamberger@users.noreply.github.com>
2020-10-12 16:49:36 -0700

    Added comments

--- RUN_ME.m
+++ RUN_ME.m
@@ -1,3 +1,7 @@
+%Copyright 2020 LabMonti.  Written by Nathan Bamberger.  This work is 
+%licensed under the Creative Commons Attribution-NonCommercial 4.0 
+%International License. To view a copy of this license, visit 
+%http://creativecommons.org/licenses/by-nc/4.0/.  
 
-
+%Adds all folders in this package to the MATLAB path
 addpath(genpath('.'));
